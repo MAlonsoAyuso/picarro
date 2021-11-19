6af6611dace8c6f0b4437006d283dcd19fc862df
Rasmus Einarsson <mr@rasmuseinarsson.se>
2021-11-19 19:09:15 +0100

    Add code to split data file between chambers

--- picarro.py
+++ picarro.py
@@ -17,3 +17,24 @@ def parse(path):
 def _parse_timestamp(d):
     timestamp_str = d["DATE"] + " " + d["TIME"]
     return timestamp_str.apply(datetime.datetime.strptime, args=(_TIMESTAMP_FORMAT,))
+
+
+def _drop_data_between_valves(data):
+    # Column "solenoid_valves" is sometimes noninteger for a short time when switching
+    # from one valve to the next. Let's drop these data as they cannot be connected
+    # to a chamber.
+    valve_num = data["solenoid_valves"]
+    is_between_valves = valve_num.astype(int) != valve_num
+    return data[~is_between_valves]
+
+
+def _split_iter(d):
+    d = d.pipe(_drop_data_between_valves)
+    valve_just_changed = d["solenoid_valves"].diff() != 0
+    valve_change_count = valve_just_changed.cumsum()
+    for i, g in d.groupby(valve_change_count):
+        yield g
+
+
+def split(d):
+    return list(_split_iter(d))
--- test_picarro.py
+++ test_picarro.py
@@ -14,3 +14,9 @@ def test_require_well_formatted_timestamps():
 def test_require_unique_timestamps():
     with pytest.raises(ValueError):
         picarro.parse("example_data/duplicate_timestamp.dat")
+
+
+def test_split():
+    d = picarro.parse("example_data/example.dat")
+    for part in picarro.split(d):
+        assert len(part["solenoid_valves"].unique()) == 1
