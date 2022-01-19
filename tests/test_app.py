2f486cfee7665490234e867403f95e2d5a2b6d73
Rasmus Einarsson <mr@rasmuseinarsson.se>
2022-01-19 17:41:33 +0100

    Merge config and app test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,48 @@
-from operator import le
 from pathlib import Path
+import shutil
 import pytest
 import picarro.app
-from picarro.config import AppConfig
+from picarro.config import AppConfig, UserConfig, ReadConfig, FitConfig, OutputConfig
 from picarro.read import Measurement, PicarroColumns
 
 config_example_src = Path(__file__).absolute().parent / "config_example.toml"
+assert config_example_src.exists()
 
 
 @pytest.fixture
 def app_config(tmp_path: Path) -> AppConfig:
     return AppConfig.from_toml(config_example_src)
+
+
+def test_create_config(tmp_path: Path):
+    config_path = tmp_path / "config.toml"
+    shutil.copyfile(config_example_src, config_path)
+
+    conf = AppConfig.from_toml(config_path)
+    expected_conf = AppConfig(
+        src_dir=config_path.parent,
+        results_subdir=config_path.stem,
+        user=UserConfig(
+            ReadConfig(
+                src="../example_data/adjacent_files/**/*.dat",
+                columns=["N2O", "CH4"],
+                max_gap=5,
+                min_length=1080,
+                max_length=None,
+            ),
+            FitConfig(
+                method="linear",
+                t0=480,
+                t0_margin=120,
+                A=0.25,
+                Q=4.16e-6,
+                V=50e-3,
+            ),
+            OutputConfig(),
+        ),
+    )
+
+    assert conf == expected_conf
 
 
 def test_iter_measurements(app_config: AppConfig):
@@ -36,4 +68,3 @@ def test_iter_measurements(app_config: AppConfig):
     ]
 
     assert seen_measurements == expected_measurements
-
